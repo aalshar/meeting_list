44d2881febc179865209972b80767e63f5e2e177
Greg DeLozier <gregdelozier@gmail.com>
2018-02-07 22:35:35 -0500

    Mongo modules

--- tasks.py
+++ tasks.py
@@ -8,9 +8,8 @@ client = MongoClient("mongodb://greg:happy121@ds221258.mlab.com:21258/cloudapps"
 db = client.cloudapps
 current_tasks = db.current_tasks
 
-def save_task(task):
-    task_id = current_tasks.insert_one(task).inserted_id
-    return str(task_id)
+def get_task_list():
+    return list(current_tasks.find())
 
 def get_task(task_id):
     # Convert from string to ObjectId:
@@ -18,6 +17,22 @@ def get_task(task_id):
     task = current_tasks.find_one({'_id': object_id})
     return task
 
-def get_task_list():
-    return list(current_tasks.find())
+def save_task(task):
+    task_id = current_tasks.insert_one(task).inserted_id
+    return str(task_id)
+
+def delete_task(task_id):
+    object_id = ObjectId(task_id)
+    task = current_tasks.delete_one({'_id': object_id})    
+
+def update_task(task_id, description=None, status=None):
+    if description:
+        update = {'$set':{'description':description}}
+        object_id = ObjectId(task_id)
+        current_tasks.update_one({'_id': object_id}, update)    
+    if status:
+        update = {'$set':{'status':status}}
+        object_id = ObjectId(task_id)
+        current_tasks.update_one({'_id': object_id}, update)    
+
 
--- test_tasks.py
+++ test_tasks.py
@@ -6,8 +6,16 @@ def test_get_tasks():
     task_list = tasks.get_task_list()
     assert type(task_list) is list
 
+def test_get_task():
+    task_id = tasks.save_task({'description' : "This is a test task.", 'status' : "1"})
+    assert type(task_id) is str
+    task = tasks.get_task(task_id)
+    print(task)
+    print(type(task))
+    assert(task['description'] == "This is a test task.")
+
 def test_save_task():
-    tasks.save_task({'description' : "Do something", 'status' : "1"})
+    tasks.save_task({'description' : "Do something worth saving", 'status' : "1"})
     task_list = tasks.get_task_list()
     assert type(task_list) is list
     found = False
@@ -17,14 +25,31 @@ def test_save_task():
             found = True
     assert found
 
-def test_get_task():
-    task_id = tasks.save_task({'description' : "This is a test task.", 'status' : "1"})
-    assert type(task_id) is str
-    task = tasks.get_task(task_id)
-    print(task)
-    print(type(task))
-    assert(task['description'] == "This is a test task.")
+def test_delete_task():
+    task_id = tasks.save_task({'description' : "This is a deletable task.", 'status' : "1"})
+    task_list = tasks.get_task_list()
+    found = False
+    for task in task_list:
+        if "deletable" in task['description']:
+            found = True 
+    assert found
+    tasks.delete_task(task_id)
+    task_list = tasks.get_task_list()
+    found = False
+    for task in task_list:
+        if "deletable" in task['description']:
+            found = True 
+    assert not found
 
+def test_update_task():
+    task_id = tasks.save_task({'description' : "Do something worth updating", 'status' : "1"})
+    tasks.update_task(task_id, description="This has been updated")
+    task = tasks.get_task(task_id)
+    assert "updated" in task['description']
+    tasks.update_task(task_id, status="0")
+    task = tasks.get_task(task_id)
+    assert "0" in task['status']
+    
 def test_mongo_client():
     assert type(tasks.client) is MongoClient
     assert tasks.db
